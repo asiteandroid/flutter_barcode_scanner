f0a3e861fbbf4f4b242a19121422d83784fa68a6
Ankita Mehta <amehta@asite.com>
2022-09-14 17:34:28 +0530

    Register manually

--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
@@ -1,14 +1,18 @@
 package com.amolg.flutterbarcodescannerexample;
 
 import android.os.Bundle;
-import io.flutter.embedding.android.FlutterFragmentActivity;
+
+import com.amolg.flutterbarcodescanner.FlutterBarcodeScannerPlugin;
+
+import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class EmbeddingV1Activity extends FlutterFragmentActivity {
+public class EmbeddingV1Activity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
+//        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
+        FlutterBarcodeScannerPlugin.registerWith(this.registrarFor("com.amolg.flutterbarcodescanner.FlutterBarcodeScannerPlugin"));
     }
 }
--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
@@ -1,8 +1,8 @@
 package com.amolg.flutterbarcodescannerexample;
 
-import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterFragmentActivity {
+public class MainActivity extends FlutterActivity {
   // You can keep this empty class or remove it. Plugins on the new embedding
   // now automatically registers plugins.
 }
