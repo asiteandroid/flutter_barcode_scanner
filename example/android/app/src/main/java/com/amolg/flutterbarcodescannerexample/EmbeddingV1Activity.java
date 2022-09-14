4b9c2b58b5f52a37f2e7d85188a3a1f629695747
Ankita Mehta <amehta@asite.com>
2022-09-14 17:40:11 +0530

    Revert all the changes

--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
@@ -1,9 +1,6 @@
 package com.amolg.flutterbarcodescannerexample;
 
 import android.os.Bundle;
-
-import com.amolg.flutterbarcodescanner.FlutterBarcodeScannerPlugin;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -12,7 +9,6 @@ public class EmbeddingV1Activity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
-        FlutterBarcodeScannerPlugin.registerWith(this.registrarFor("com.amolg.flutterbarcodescanner.FlutterBarcodeScannerPlugin"));
+        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
     }
 }
