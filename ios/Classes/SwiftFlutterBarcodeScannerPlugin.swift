a213c315cee98d676cb309c71ee598046e4aa50f
asiteandroid <58687792+asiteandroid@users.noreply.github.com>
2022-12-02 14:32:46 +0530

    Update SwiftFlutterBarcodeScannerPlugin.swift
    Updating code on cancelling QR code window

--- ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
+++ ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
@@ -503,12 +503,12 @@ class BarcodeScannerViewController: UIViewController {
     @IBAction private func cancelButtonClicked() {
         if SwiftFlutterBarcodeScannerPlugin.isContinuousScan{
             self.dismiss(animated: true, completion: {
-                SwiftFlutterBarcodeScannerPlugin.onBarcodeScanReceiver(barcode: "-1")
+                SwiftFlutterBarcodeScannerPlugin.onBarcodeScanReceiver(barcode: "-2")
             })
         }else{
             if self.delegate != nil {
                 self.dismiss(animated: true, completion: {
-                    self.delegate?.userDidScanWith(barcode: "-1")
+                    self.delegate?.userDidScanWith(barcode: "-2")
                 })
             }
         }
